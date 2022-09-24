79ab391ceff439ab94bec391bcfb6faa6d258d76
mechztcc <dojathan@gmail.com>
2022-09-24 15:56:01 -0400

    feat: added debounceTime when try to click multiple times on like

--- src/app/shared/components/photo-frame/photo-frame.component.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.ts
@@ -1,22 +1,46 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-photo-frame',
   templateUrl: './photo-frame.component.html',
   styleUrls: ['./photo-frame.component.scss'],
 })
-export class PhotoFrameComponent implements OnInit {
+export class PhotoFrameComponent implements OnInit, OnDestroy {
   @Output() liked: EventEmitter<void> = new EventEmitter();
 
   @Input() src: string = '';
   @Input() description: string = '';
   @Input() likes = 0;
 
+  private debounceSubject: Subject<void> = new Subject();
+  private unsubscribe: Subject<void> = new Subject();
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.debounceSubject
+      .asObservable()
+      .pipe(debounceTime(500))
+      .pipe(takeUntil(this.unsubscribe))
+      .subscribe(() => {
+        this.liked.emit();
+      });
+  }
+  ngOnDestroy(): void {
+    this.unsubscribe.next();
+    this.unsubscribe.complete();
+  }
 
   like() {
-    this.liked.emit();
+    this.debounceSubject.next();
   }
 }
